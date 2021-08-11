5bc2ebc99aa48e45c30d1be1bc61b550471d1bc3
shoshanimayan <mshoshan@uci.edu>
2021-08-10 22:38:54 -0700

    add invisible walls and particles
    just credits and testing

--- test/Assets/Internal/Scripts/Base/ElementHandlers/Base_WaterHandler.cs
+++ test/Assets/Internal/Scripts/Base/ElementHandlers/Base_WaterHandler.cs
@@ -5,13 +5,7 @@ using UnityEngine;
 public class Base_WaterHandler : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Awake()
     {
         
     }
--- test/Assets/Internal/Scripts/Base/Interactables/Water/Base_WaterColor.cs
+++ test/Assets/Internal/Scripts/Base/Interactables/Water/Base_WaterColor.cs
@@ -11,7 +11,7 @@ public class Base_WaterColor : MonoBehaviour
     private ParticleSystem _particleSystem;
     void Awake()
     {
-        _particleSystem = gameObject.GetComponent<ParticleSystem>();
+        _particleSystem = GetComponent<ParticleSystem>();
         ApplyColor(false);
     }
 
@@ -33,8 +33,10 @@ public class Base_WaterColor : MonoBehaviour
         gameObject.GetComponent<Renderer>().material = _materials[index];
         if (smoke)
         {
+            //ParticleSystemRenderer pr = _particleSystem.GetComponent<ParticleSystemRenderer>();
             ParticleSystem.MainModule main = _particleSystem.main;
             main.startColor = new ParticleSystem.MinMaxGradient(_colors[index]);
+           // pr.material= _materials[index]; 
             _particleSystem.Play();
 
         }
